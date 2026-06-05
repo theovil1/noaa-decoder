59ad943371dffa3e7374b4bcf4a4f892216d310e
theovil1 <288923546+theovil1@users.noreply.github.com>
2026-06-05 18:38:52 +0200

    fix: fix ENHANCE_H header guard typo

--- include/enhance.h
+++ include/enhance.h
@@ -1,10 +1,16 @@
 #ifndef ENHANCE_H
-#define ENHANCE H
+#define ENHANCE_H
 
 #include <stdint.h>
+
+typedef struct {
+    uint8_t r, g, b;
+} RGB;
 
 void enhance_contrast(uint8_t *pixels, int num_pixels);
 
 void enhance_median(uint8_t *pixels, int width, int height);
+
+void enhance_create_thermal_lut(RGB *lut);
 
 #endif
